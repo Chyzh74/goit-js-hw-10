fbbb5fe0b46754fc81b3cac3332deaae5d8ce696
Oleh <choleg22@gmail.com>
2023-04-23 10:49:24 +0300

    ppt

---
+++ src/js/03-promises.js
@@ -0,0 +1,37 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", onGeneratorPromise);
+
+function onGeneratorPromise(evt) {
+  evt.preventDefault();
+  const delay = parseInt(form.elements.delay.value);
+  const step = parseInt(form.elements.step.value);
+  const amount = parseInt(form.elements.amount.value);
+  for (let i = 1; i <= amount; i++) {
+    const position = i;
+    const promiseDelay = delay + (i - 1) * step;
+    createPromise(position, promiseDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+  evt.target.reset();
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
